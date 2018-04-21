2ce5680021817d60b2e1f2d7aa4157731f4083db
Aleksandr Alekseev <alekseevavx@gmail.com>
2018-04-21 19:59:05 +0300

    move MTRFSerial function to file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const AccessoryUtil = require('./accessories');
 const SerialPort = require('serialport');
 const addNooLiteCharacteristics = require('./lib/customCharacteristics');
 const NooLiteSerialParser = require('./lib/NooLiteSerialParser');
+const MTRFSerial = require('./lib/MTRFSerial');
 
 
 let PLUGIN_NAME = pjson.name,
@@ -105,58 +106,6 @@ class NooLitePlatform {
     serialPort.on('error', function(err) {
       platform.log.error('Serial port error: ', err)
     })
-
-    function MTRFSerial (serial) {
-        this._serial = serial;
-        this._queue = [];
-        this.requestTtl = 2000;
-        this._busy = false;
-        this._current = null;
-        var device = this;
-        serialPort.nlParser.on('nlres', function (nlRes) {
-            if (!device._current) return;
-
-            const [nlReq, callback, ...other] = device._current;
-
-            if (nlReq.isEqual(nlRes)) {
-              device._current[1](null, nlRes);
-            }
-            device.processQueue();
-        });
-
-        // переодически пробегаемся по всем таскам в очереди, и удаляем старые,
-        // на которые так и не пришел ответ
-        setInterval(() => {
-          this._queue = this._queue.filter((task) => {
-            const [nlReq, callback, createdTime] = task;
-            if ((Date.now() - createdTime) < this.requestTtl) {
-              return true;
-            } else {
-              callback(new Error('Timeout to get response from MTRF'))
-              return false;
-            }
-          })
-        }, 5000)
-    }
-
-    MTRFSerial.prototype.send = function (nlReq, callback) {
-        this._queue.push([nlReq, callback, Date.now()]);
-        if (this._busy) return;
-        this._busy = true;
-        this.processQueue();
-    };
-
-    MTRFSerial.prototype.processQueue = function () {
-        var next = this._queue.shift();
-
-        if (!next) {
-            this._busy = false;
-            return;
-        }
-
-        this._current = next;
-        this._serial.write(next[0].toBytes());
-    };
 
     serialPort.mtrfSerial = new MTRFSerial(serialPort);
 
